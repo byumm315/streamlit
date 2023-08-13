698ffb29152a85351a5eca431f656e5742ad038e
박유미 <81951313+byumm315@users.noreply.github.com>
2023-08-13 18:53:37 +0900

    Update aa.py

--- aa.py
+++ aa.py
@@ -30,7 +30,7 @@ st.plotly_chart(fig4)
 st.subheader('The Bar plots of Wind data')
 v2_list = ['섭씨온도','절대온도','이슬점','상대습도','대기압','포화증기압','실제증기압','증기압부족량',
               '수증기함량','공기밀도','풍향','풍속']
-vari2 = st.selectbox(label = "Choose a Variable", options = v2_list,key=1)
+vari2 = st.selectbox(label = "Choose a Variable", options = v2_list)
 title = f"The Bar plot of Month, Time Zone and {vari2}"
 fig = px.bar(data, x = '월', y = vari2,color="시간대", barmode = 'group',title=title)
 st.plotly_chart(fig)
