0695039b1d7f0af121088430a72c5724ca5c6925
박유미 <81951313+byumm315@users.noreply.github.com>
2023-08-13 19:26:18 +0900

    Update pandas_app3.py

--- pages/pandas_app3.py
+++ pages/pandas_app3.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import date
 
-df = pd.read_csv("data_elements/avocado.csv")
+df = pd.read_csv("avocado.csv")
 df['date'] = df['date'].apply(lambda x: date.fromisoformat(x))
 
 # [TODO] 달력창이 뜨는 수정가능한 테이블 삽입
