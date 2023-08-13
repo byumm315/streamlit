886d828ebd38e4afb33304b3aed5dddf600b128d
박유미 <81951313+byumm315@users.noreply.github.com>
2023-08-13 19:26:01 +0900

    Update pandas_app2.py

--- pages/pandas_app2.py
+++ pages/pandas_app2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import date
 
-df = pd.read_csv("data_elements/avocado.csv")
+df = pd.read_csv("avocado.csv")
 df['date'] = df['date'].apply(lambda x: date.fromisoformat(x))
 
 # [TODO] 테이블을 수정 가능한 테이블로 출력해봅니다.
