3a9501f08cce135ba36987ead534ea576f2e0131
박유미 <81951313+byumm315@users.noreply.github.com>
2023-08-15 16:12:09 +0900

    Update Select Topic.py

--- pages/Select Topic.py
+++ pages/Select Topic.py
@@ -15,5 +15,6 @@ title = f"Slect topic: {var1}"
 
 st.dataframe(data[data['topic']==var1])
 y_values=data[data['topic']==var1]['title'].value_counts().to_frame().reset_index(drop=False)
+st.dataframe(y_values)
 fig = px.bar(y_values, x = 'index', y = 'title', title=title)
 st.plotly_chart(fig)
